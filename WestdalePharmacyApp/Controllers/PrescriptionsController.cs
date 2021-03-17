7b488cba848bd966b3a52e068852eb26baafbc81
mesut <mesubaltaci@hotmail.com>
2021-03-16 22:43:15 -0400

    created prescription

--- WestdalePharmacyApp/Controllers/PrescriptionsController.cs
+++ WestdalePharmacyApp/Controllers/PrescriptionsController.cs
@@ -42,13 +42,17 @@ namespace WestdalePharmacyApp.Controllers
             _signInManager = signInManager;
         }
 
+        [Authorize(Roles = "Client")]
         // GET: Prescriptions
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Prescriptions.Include(p => p.User);
+            var user = await _userManager.GetUserAsync(User);
+            var applicationDbContext = _context.Prescriptions.Where(o => o.UserId == user.Id)
+                .Include(p => p.User);
             return View(await applicationDbContext.ToListAsync());
         }
 
+        [Authorize(Roles = "Client")]
         // GET: Prescriptions/Details/5
         public async Task<IActionResult> Details(Guid? id)
         {
@@ -68,6 +72,7 @@ namespace WestdalePharmacyApp.Controllers
             return View(prescription);
         }
 
+        [Authorize(Roles = "Client")]
         // GET: Prescriptions/Create
         public IActionResult Create()
         {
@@ -75,6 +80,7 @@ namespace WestdalePharmacyApp.Controllers
             return View();
         }
 
+        [Authorize(Roles = "Client")]
         // POST: Prescriptions/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -93,6 +99,8 @@ namespace WestdalePharmacyApp.Controllers
             return View(prescription);
         }
 
+
+        [Authorize(Roles = "Client")]
         // GET: Prescriptions/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
         {
@@ -110,6 +118,7 @@ namespace WestdalePharmacyApp.Controllers
             return View(prescription);
         }
 
+        [Authorize(Roles = "Client")]
         // POST: Prescriptions/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -117,11 +126,19 @@ namespace WestdalePharmacyApp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(Guid id, [Bind("PrescriptionId,Refill,ImageFile,Status,CreationTime,UpdatedTime,SpecialInstruction,UserId")] Prescription prescription)
         {
+
             if (id != prescription.PrescriptionId)
             {
                 return NotFound();
             }
-
+            var user = await _userManager.GetUserAsync(User);
+            var tempPrescription = await _context.Prescriptions.AsNoTracking().Where(o => o.PrescriptionId == id).FirstOrDefaultAsync();             prescription.ImageFile = tempPrescription.ImageFile;
+            prescription.UpdatedTime = DateTimeOffset.Now;
+            prescription.ImageFile = tempPrescription.ImageFile;
+            prescription.CreationTime = tempPrescription.CreationTime;
+            prescription.Status = tempPrescription.Status;
+            prescription.RefillAvailable = tempPrescription.RefillAvailable;
+            prescription.UserId = user.Id;
             if (ModelState.IsValid)
             {
                 try
@@ -146,6 +163,7 @@ namespace WestdalePharmacyApp.Controllers
             return View(prescription);
         }
 
+        [Authorize(Roles = "Client")]
         // GET: Prescriptions/Delete/5
         public async Task<IActionResult> Delete(Guid? id)
         {
@@ -165,6 +183,7 @@ namespace WestdalePharmacyApp.Controllers
             return View(prescription);
         }
 
+        [Authorize(Roles = "Client")]
         // POST: Prescriptions/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -233,6 +252,172 @@ namespace WestdalePharmacyApp.Controllers
                 bytes = Convert.FromBase64String(sBase64String);
             }
             return bytes;
+        }
+
+
+
+        [Authorize(Roles = "Client")]
+        // GET: Prescriptions/Refill/5
+        public async Task<IActionResult> Refill(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var prescription = await _context.Prescriptions.FindAsync(id);
+            if (prescription == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", prescription.UserId);
+            return View(prescription);
+        }
+
+        [Authorize(Roles = "Client")]
+        // POST: Prescriptions/Refill/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Refill(Guid id, [Bind("PrescriptionId,Refill,ImageFile,Status,CreationTime,UpdatedTime,SpecialInstruction,UserId")] Prescription prescription)
+        {
+
+            if (id != prescription.PrescriptionId)
+            {
+                return NotFound();
+            }
+            var tempPrescription = await _context.Prescriptions.AsNoTracking().Where(o => o.PrescriptionId == id).FirstOrDefaultAsync(); prescription.ImageFile = tempPrescription.ImageFile;
+            prescription.UpdatedTime = DateTimeOffset.Now;
+            prescription.ImageFile = tempPrescription.ImageFile;
+            prescription.CreationTime = tempPrescription.CreationTime;
+            prescription.Status = "Completed";
+            prescription.RefillAvailable = 0;
+           
+            
+            var requestedPrescription = tempPrescription;
+            requestedPrescription.CreationTime = DateTimeOffset.Now;
+            requestedPrescription.Status = "Refill Requested";
+            requestedPrescription.UpdatedTime = null;
+            requestedPrescription.SpecialInstruction = prescription.SpecialInstruction;
+            requestedPrescription.RefillAvailable = tempPrescription.RefillAvailable - 1;
+            //requestedPrescription.TimesRefill = 
+            requestedPrescription.PrescriptionId = new Guid();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(prescription);
+                    await _context.SaveChangesAsync();
+
+                    _context.Add(requestedPrescription);
+                    await _context.SaveChangesAsync();
+
+                    var user = await _userManager.GetUserAsync(User);
+                    await _emailSender.SendEmailAsync(user.Email, "Refill Request", "Your refill request successfully got it");
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PrescriptionExists(prescription.PrescriptionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", prescription.UserId);
+            return View(prescription);
+        }
+
+
+
+        [Authorize(Roles = "Admin")]
+        // GET: Prescriptions for admin
+        public async Task<IActionResult> IndexA()
+        {
+            var applicationDbContext = _context.Prescriptions.Include(p => p.User);
+
+            var prescriptionViewModel =
+                    from u in _context.Users
+                    join p in _context.Prescriptions on u.Id equals p.UserId
+                    where p.Status.Equals("Not Set")
+                    select new PrescriptionViewModel { ApplicationUser = u, Prescription = p };
+
+            return View(await prescriptionViewModel.ToListAsync());
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        // GET: Prescriptions/Edit/5
+        public async Task<IActionResult> EditA(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //var prescription = await _context.Prescriptions.FindAsync(id);
+            var prescription =
+                    from u in _context.Users
+                    join p in _context.Prescriptions on u.Id equals p.UserId
+                    where p.PrescriptionId ==id
+                    select new PrescriptionViewModel { ApplicationUser = u, Prescription = p };
+            if (prescription == null)
+            {
+                return NotFound();
+            }
+            //ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", prescription.UserId);
+            return View( await prescription.FirstOrDefaultAsync());
+        }
+
+        [Authorize(Roles = "Admin")]
+        // POST: Prescriptions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditA(Guid id, [Bind("PrescriptionId,Refill,ImageFile,Status,CreationTime,UpdatedTime,SpecialInstruction,UserId")] Prescription prescription)
+        {
+
+            if (id != prescription.PrescriptionId)
+            {
+                return NotFound();
+            }
+            var user = await _userManager.GetUserAsync(User);
+            var tempPrescription = await _context.Prescriptions.AsNoTracking().Where(o => o.PrescriptionId == id).FirstOrDefaultAsync(); prescription.ImageFile = tempPrescription.ImageFile;
+            prescription.UpdatedTime = DateTimeOffset.Now;
+            prescription.ImageFile = tempPrescription.ImageFile;
+            prescription.CreationTime = tempPrescription.CreationTime;
+            prescription.Status = tempPrescription.Status;
+            prescription.RefillAvailable = tempPrescription.RefillAvailable;
+            prescription.UserId = user.Id;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(prescription);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PrescriptionExists(prescription.PrescriptionId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", prescription.UserId);
+            return View(prescription);
         }
 
     }
